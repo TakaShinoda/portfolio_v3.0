7f37fd7869f67f91f413e5888b95be2105055b09
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-22 21:56:16 +0900

    fix: ルーティングを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { TopPage } from './components/TopPage'
+import { CurriculumVitae } from './components/CurriculumVitae'
+import { Social } from './components/Social'
+import './App.css'
 
-function App() {
+
+export const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <TopPage />
+          </Route>
+          <Route exact path="/curriculum">
+            <CurriculumVitae />
+          </Route>
+          <Route exact path="/social">
+            <Social />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
-
-export default App;
