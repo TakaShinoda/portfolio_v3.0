00f8f0ded966f3fbb087cd507dc4d48a36f2d1ec
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-23 16:17:24 +0900

    fix: ルーティングを追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { TopPage } from './components/TopPage'
 import { CurriculumVitae } from './components/CurriculumVitae'
+import { Development } from './components/Development'
 import { Social } from './components/Social'
 import './App.css'
 
@@ -16,6 +17,9 @@ export const App: FC = () => {
           </Route>
           <Route exact path="/curriculum">
             <CurriculumVitae />
+          </Route>
+          <Route exact path="/development">
+            <Development />
           </Route>
           <Route exact path="/social">
             <Social />
