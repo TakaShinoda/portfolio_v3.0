69d7c690e01d073d67272bd92071d07cf82cc25d
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-26 16:41:19 +0900

    fix: contentを修正

--- src/components/CurriculumVitae.tsx
+++ src/components/CurriculumVitae.tsx
@@ -21,7 +21,7 @@ export const CurriculumVitae: FC = () => {
       <VerticalTimeline>
           <TimeLineShool date="2014/04 - 2018/03" title="宮崎大学" subtitle="工学部 情報システム工学科" />
           <TimeLineShool date="2018/04 - 2020/03"  title="宮崎大学大学院" subtitle="機械・情報系コース 情報システム工学分野" content="画像処理(プロジェクションマッピング)の研究に取り組んでいました。" />
-          <TimeLine date="2020/04 - present" title="株式会社イデア・レコード" subtitle="エンジニア" content="主にHTML, CSS, CakePHPを用いたwebページの作成" />
+          <TimeLine date="2020/04 - present" title="株式会社イデア・レコード" subtitle="エンジニア" content="主にHTML, CSS, CakePHP, Vue.jsを用いたwebページの作成" />
       </VerticalTimeline>
     </>
   )
