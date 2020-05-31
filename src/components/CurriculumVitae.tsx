b2070a3128ea32d0fa580bad1b371f21afbcfde2
Takahiro <tkhr_621@docomo.ne.jp>
2020-05-31 18:09:39 +0900

    fix: タイムライン表示を降順に変更

--- src/components/CurriculumVitae.tsx
+++ src/components/CurriculumVitae.tsx
@@ -3,7 +3,6 @@ import { VerticalTimeline } from 'react-vertical-timeline-component'
 import { TimeLineShool } from './TimeLineShool'
 import { TimeLine } from './TimeLine'
 
-
 export const CurriculumVitae: FC = () => {
   return (
     <>
@@ -19,9 +18,23 @@ export const CurriculumVitae: FC = () => {
       </h2>
       <br />
       <VerticalTimeline>
-          <TimeLineShool date="2014/04 - 2018/03" title="宮崎大学" subtitle="工学部 情報システム工学科" />
-          <TimeLineShool date="2018/04 - 2020/03"  title="宮崎大学大学院" subtitle="機械・情報系コース 情報システム工学分野" content="画像処理(プロジェクションマッピング)の研究に取り組んでいました。" />
-          <TimeLine date="2020/04 - present" title="株式会社イデア・レコード" subtitle="エンジニア" content="主にHTML, CSS, CakePHP, Vue.jsを用いたwebページの作成" />
+        <TimeLine
+          date="2020/04 - present"
+          title="株式会社イデア・レコード"
+          subtitle="エンジニア"
+          content="主にHTML, CSS, CakePHP, Vue.jsを用いたwebページの作成"
+        />
+        <TimeLineShool
+          date="2018/04 - 2020/03"
+          title="宮崎大学大学院"
+          subtitle="機械・情報系コース 情報システム工学分野"
+          content="画像処理(プロジェクションマッピング)の研究に取り組んでいました。"
+        />
+        <TimeLineShool
+          date="2014/04 - 2018/03"
+          title="宮崎大学"
+          subtitle="工学部 情報システム工学科"
+        />
       </VerticalTimeline>
     </>
   )
