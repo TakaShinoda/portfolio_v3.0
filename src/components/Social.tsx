6e34b3241101d753e18ed32f938476172fd3a96d
Takahiro <tkhr_621@docomo.ne.jp>
2020-06-02 23:40:51 +0900

    Fix: emotionによるスタイル定義

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,5 +1,5 @@
-import React, { FC, Fragment } from 'react'
 /** @jsx jsx */
+import React, { FC, Fragment } from 'react'
 import { css, jsx } from '@emotion/core'
 
 export const Social: FC = () => {
