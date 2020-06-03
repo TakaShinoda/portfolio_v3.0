6b32b1729ebde58b456daf73df4e47f89a14104f
Takahiro <tkhr_621@docomo.ne.jp>
2020-06-03 22:45:48 +0900

    Fix: emotionによるスタイル定義に変更

--- src/components/TopPage.tsx
+++ src/components/TopPage.tsx
@@ -1,38 +1,49 @@
+/** @jsx jsx */
 import React, { FC } from 'react'
 import { useHistory } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
+import { css, jsx } from '@emotion/core'
 
 export const TopPage: FC = () => {
   const history = useHistory()
+
+  const liStyle = css({
+    color: "#fff"
+  })
+
+  const linkStyle = css({
+    color: '#2196f3',
+  })
+
   return (
-    <>
+    <React.Fragment>
       <h1>Shinoda Takahiro</h1>
       <ul>
-        <li>
+        <li css={liStyle}>
           <Button
             disableRipple={true}
             style={{ backgroundColor: 'transparent' }}
           >
-            <h2 className="link" onClick={() => history.push('/curriculum')}>Info</h2>
+            <h2 css={linkStyle} onClick={() => history.push('/curriculum')}>Info</h2>
           </Button>
         </li>
-        <li>
+        <li css={liStyle}>
           <Button
             disableRipple={true}
             style={{ backgroundColor: 'transparent' }}
           >
-            <h2 className="link" onClick={() => history.push('/development')}>Dev</h2>
+            <h2 css={linkStyle} onClick={() => history.push('/development')}>Dev</h2>
           </Button>
         </li>
-        <li>
+        <li css={liStyle}>
           <Button
             disableRipple={true}
             style={{ backgroundColor: 'transparent' }}
           >
-            <h2 className="link" onClick={() => history.push('/social')}>SNS</h2>
+            <h2 css={linkStyle} onClick={() => history.push('/social')}>SNS</h2>
           </Button>
         </li>
       </ul>
-    </>
+    </React.Fragment>
   )
 }
